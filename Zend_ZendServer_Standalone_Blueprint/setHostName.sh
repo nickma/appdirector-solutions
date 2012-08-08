a75182d21468bf50ee6084dea811e84e9dc52af6
Nick Maiorsky <nickma@gmail.com>
2012-08-08 13:01:30 -0400

    Removed Oralce example stuff

--- Zend_ZendServer_Standalone_Blueprint/setHostName.sh
+++ Zend_ZendServer_Standalone_Blueprint/setHostName.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Use ENV_NAME if specified.
-
-# set Hostname variable
-HOST_NAME="ora-"$ENV_NAME"-oid-d1"
-
 echo $HOST_NAME
 
 # change VM hostname
@@ -20,4 +15,3 @@ sed -i "s~$IP_ADDRESS.*~$IP_ADDRESS $HOST_NAME.$DOMAIN_NAME $HOST_NAME~g" /etc/h
 HOST_NAME="$HOST_NAME"."$DOMAIN_NAME"
 
 echo "Setting hostname to $HOST_NAME"
-
